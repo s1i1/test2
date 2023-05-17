c9b5b79d4b08a86e6c4d9ed5ef65f659f8489ec9
Ilin S <slava1221fmkafunnikmiy@gmail.com>
2023-05-17 18:51:26 +0300

    created vehicleSort

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,42 @@
-import { useAppDispatch } from '@hooks/redux';
+import { VehicleCard } from '@components/VehicleCard';
+import { VehicleSort } from '@components/VehicleSort';
+import { AVAILABLE_SORTS } from '@constants/sort';
+import { useAppDispatch, useAppSelector } from '@hooks/redux';
 import { fetchVehiclesRequest } from '@store/vehicles/actions';
-import { useEffect } from 'react';
+import { getVehicles } from '@store/vehicles/selectors';
+import { VehiclesData } from '@store/vehicles/slice';
+import { useEffect, useState } from 'react';
 
 const App = () => {
   const dispatch = useAppDispatch();
 
+  const vehicles = useAppSelector(getVehicles);
+
+  const [selectedSort, setSelectedSort] = useState('');
+  const [sortedVehicles, setSortedVehicles] = useState<VehiclesData[]>([]);
+
+  const sortCallback = AVAILABLE_SORTS[selectedSort];
+
+  useEffect(() => {
+    if (!selectedSort) return;
+    setSortedVehicles((prev) => [...prev.sort(sortCallback)]);
+  }, [selectedSort, sortCallback]);
+
+  const handleChange = (value: string) => {
+    setSelectedSort(value);
+  };
+
   useEffect(() => {
     dispatch(fetchVehiclesRequest());
-  }, []);
-  return <></>;
+    setSortedVehicles([...vehicles]);
+  }, [dispatch]);
+
+  return (
+    <>
+      <VehicleSort onChange={handleChange} />
+      <VehicleCard vehicles={sortedVehicles} />
+    </>
+  );
 };
 
 export default App;
