8b80d7e884ea07f9421e5b440669d8c0de926eff
Ilin S <slava1221fmkafunnikmiy@gmail.com>
2023-05-17 18:38:52 +0300

    change type in saga

--- src/sagas/vehicles.ts
+++ src/sagas/vehicles.ts
@@ -1,10 +1,10 @@
 import { all, call, put, takeLatest } from 'redux-saga/effects';
-import { getVehicles } from './api/vehicles';
+import { getVehicles, VehiclesData } from './api/vehicles';
 import { fetchVehiclesRequest, fetchVehiclesSuccess } from '@store/vehicles/actions';
 
 const fetchVehiclesRequestHandler = function* () {
   try {
-    const vehiclesData: ReturnType<typeof getVehicles> = yield call(getVehicles);
+    const vehiclesData: VehiclesData[] = yield call(getVehicles);
 
     yield put(fetchVehiclesSuccess({ data: vehiclesData }));
   } catch (e) {
